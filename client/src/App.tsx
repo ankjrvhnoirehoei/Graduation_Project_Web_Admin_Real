b5affa36d893084a680f7fa893448e14b33284ef
ankjrvhnoirehoei <166099409+ankjrvhnoirehoei@users.noreply.github.com>
2025-06-21 18:59:33 +0700

    refresh

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,8 @@
 import * as React from 'react'
 import { Admin, Resource } from 'react-admin'
-import { UserList } from './components/users/UserList'
-import { UserShow } from './components/users/UserShow'
-import { dataProvider } from './dataProvider'
-
 function App() {
   return (
-    <Admin dataProvider={dataProvider}>
-      <Resource 
-        name="users/admin/all" 
-        list={UserList} 
-        show={UserShow}
-        options={{ label: 'Users' }}
-      />
-    </Admin>
+  <h1></h1>
   )
 }
 
